3f34f0f9223a299479383714b85e9cd66d3a965d
BiggerSTG <sgarg53@asu.edu>
2025-02-18 05:41:04 -0700

    Added Flask Server
    Added a simple flask server for testing purposes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from crewai import Crew
 from agents import weather_agent, weather_task, map_agent, map_task
+from flask import Flask, request
+
+app = Flask(__name__)
 
 # Create a Crew to execute the task
 crew = Crew(
@@ -9,9 +12,17 @@ crew = Crew(
     verbose=True
 )
 
-# Execute the crew with input cities
-result = crew.kickoff(inputs={"cities": ["New York", "London", "Tokyo"], "location": "33.416151, -111.910912", "radius": 1000, "categories": "tourist_attractions"})
-
 # Print the result
 print("############")
-print(result)
+#print(result)
+
+@app.route('/run', methods=['GET'])
+def predict():
+    # Execute the crew with input cities
+    result = crew.kickoff(
+        inputs={"cities": ["New York", "London", "Tokyo"], "location": "33.416151, -111.910912", "radius": 1000,
+                "categories": "tourist_attractions"})
+    return result
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5001)
